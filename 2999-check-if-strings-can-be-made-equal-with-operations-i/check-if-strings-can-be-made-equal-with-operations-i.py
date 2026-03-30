af2107db59860454fbabf3f994b7f5368670cfb0
Jerome Paulo Mira <jeromepaulomira@gmail.com>
2026-03-30 19:10:22 +0800

    Time: 0 ms (100.00%) | Memory: 19.2 MB (73.74%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,6 +1,7 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
         n = len(s1)
+        #set dictionary which count the number for odd and even indices of s1 and s2
         e1, o1 = defaultdict(int), defaultdict(int)
         e2, o2 = defaultdict(int), defaultdict(int)
 
@@ -11,4 +12,10 @@ class Solution:
             else:
                 o1[s1[i]] += 1
                 o2[s2[i]] += 1
+
+        """
+        if the count of the characters are the same based on the indices, 
+        return true, else false
+        """
+        
         return e1 == e2 and o1 == o2
