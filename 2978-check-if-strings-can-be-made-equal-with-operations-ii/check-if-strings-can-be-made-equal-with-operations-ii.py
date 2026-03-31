0ab3bc458615451dd11050fc6d26f9794d6f1665
Jerome Paulo Mira <jeromepaulomira@gmail.com>
2026-03-31 13:27:01 +0800

    Time: 151 ms (36.73%) | Memory: 20.1 MB (77.05%) - LeetSync

---
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        n = len(s1)
+        e1, o1 = defaultdict(int), defaultdict(int)
+        e2, o2 = defaultdict(int), defaultdict(int)
+
+        for i in range(n):
+            if i%2 == 0:
+                e1[s1[i]] += 1  
+                e2[s2[i]] += 1
+            else:
+                o1[s1[i]] += 1
+                o2[s2[i]] += 1
+        return e1 == e2 and o1 == o2
+
+        
